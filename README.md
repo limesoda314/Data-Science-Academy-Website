f77be60894d131d2cc6e90266459e5bc18e27e0c
limesoda314 <amiya017@ucr.edu>
2022-06-25 21:35:31 -0700

    Basic format for the website. Sections of the website are color coded for development and are not final

---
+++ README.md
@@ -0,0 +1,2 @@
+## Data Science Academy Website 
+This is the reposititory for the website for the Data Science Academy at UC Riverside.  
---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<svelte:head> 
+
+<title>DSA at UCR</title>
+
+</svelte:head>
+
+
+<body> 
+	<div class="navbar">
+		Home
+		About
+		Past Cohorts
+		DSA Leaders (maybe label it Meet the Team) 
+		Youtube 
+		Contact Us
+		Join US 
+	</div>
+
+	<div class="hero"> 
+		<div class="container"> 
+			<h1 class="center"> 
+				Data Science Academy
+			</h1>
+		</div>
+ 	</div>
+
+	
+
+	<div class="container">
+		<div class="article">
+
+			<h2> 
+				About US
+			</h2>
+			
+			<h2> 
+				Our mission 
+			</h2>
+
+			What we do 
+			Why we do it 
+
+			<h2> 
+				Past Cohorts
+			</h2>
+			Past Projects
+			Schools we worked with 
+
+
+			<h2> 
+				Future Cohorts (Schedule for future?) 
+			</h2>
+			Future Session Dates
+
+
+			<h2> 
+				Contact US
+			</h2>
+			email or link to google form 
+			<h2> 
+				Join US
+			</h2>
+			link to google form 
+			link to youtube channel (learn more) 
+
+
+		</div>
+	</div>
+
+	<div class="footer">
+		<div class="container">
+			Data Science Academy 
+		</div>
+	</div>
+
+</body>
