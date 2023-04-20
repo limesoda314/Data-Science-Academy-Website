16f3ec58d7a6ce6cfc5056bf2968180695f8fbca
unknown <limesoda314@gmail.com>
2023-04-19 21:18:43 -0700

    Added basic Carousel: https://vadimkorr.github.io/svelte-carousel/#features-use-cases

--- src/Components/Hero.svelte
+++ src/Components/Hero.svelte
@@ -1,14 +1,18 @@
 <script>
     import './../Style/Hero.css';
-    import Carousel from 'svelte-carousel'
-    import Color from './Color.svelte'
+    import Carousel from 'svelte-carousel';
+    import Color from './Color.svelte';
 </script>
 
 <div class="banner" id="Home"> 
-    <h1 class="center"> 
+    <h1 class="center" style=""> 
         DSA 2023
     </h1>
-    <!-- <Carousel
+    <div
+    class="color-container"
+    style="background-color: {color};"
+    >
+    <Carousel
         autoplay 
         autoplayDuration={5000}
     >
@@ -17,5 +21,5 @@
         <Color color="#5dca64" text="#5dca64" />
         <Color color="#49c351" text="#49c351" />
         <Color color="#35bd3e" text="#35bd3e" />
-    </Carousel> -->
+    </Carousel>
 </div>
