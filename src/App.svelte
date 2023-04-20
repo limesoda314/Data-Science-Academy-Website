b68243ea64beb665986697eb1f29c4766ec140e5
ivanneto02 <ivan.a.neto@hotmail.com>
2023-04-19 23:04:56 -0700

    Added routes, website now has multiple pages
    <Link> component to change url
    <Router> handles routes
    <Route> handles what to render on different pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,32 @@
 <script>
+
+	// Constant throughout all pages
 	import Navigation from "./Components/Navigation.svelte";
-	import Hero from "./Components/Hero.svelte";
-	import Container from "./Components/Container.svelte";
-	import Footer from "./Components/Footer.svelte"; 
-	
+
+	// Import pages necessary to route our links (i.e. /home, /about, etc)
+	import HomePage from "./routes/HomePage.svelte";
+	import AboutPage from "./routes/AboutPage.svelte";
+	import ContactPage from "./routes/ContactPage.svelte";
+	import PeoplePage from "./routes/PeoplePage.svelte";
+	import ShowCasePage from "./routes/ShowCasePage.svelte";
+
+	// Takes care of the routing
+	import { Router, Route } from "svelte-routing";
+
 </script>
 
 <svelte:head> 
 	<title>DSA at UCR</title>
 </svelte:head>
 
-<body> 
+<Router>
 	<Navigation/>
-	<Hero/>
-	<Container/>
-	<Footer/>
-</body>
+	<div>
+		<Route path="/"><HomePage /></Route>
+		<Route path="/home"><HomePage /></Route>
+		<Route paht="/about"><AboutPage /></Route>
+		<Route path="/contact"><ContactPage /></Route>
+		<Route path="/people"><PeoplePage /></Route>
+		<Route path="/showcase"><ShowCasePage /></Route>
+	</div>
+</Router>
--- src/Components/Container.svelte
+++ src/Components/Container.svelte
@@ -1,43 +1,7 @@
 <script>
     import './../Style/Container.css';
 </script>
+
 <div class="container">
-    <div class="article">
-
-        <h2> 
-            About US
-        </h2>
-        
-        <h2> 
-            Our mission 
-        </h2>
-
-        What we do 
-        Why we do it 
-
-        <h2> 
-            Past Cohorts
-        </h2>
-        Past Projects
-        Schools we worked with 
-
-
-        <h2> 
-            Future Cohorts (Schedule for future?) 
-        </h2>
-        Future Session Dates
-
-
-        <h2> 
-            Contact US
-        </h2>
-        email or link to google form 
-        <h2> 
-            Join US
-        </h2>
-        link to google form 
-        link to youtube channel (learn more) 
-
-
-    </div>
+    <slot></slot>
 </div>
