b8d84c6059f47a68e30792111d135b7c4b670504
ivanneto02 <ivan.a.neto@hotmail.com>
2023-04-24 13:03:07 -0700

    Created some content for the HomePage.
    Currently I am using placeholder images, as I am unsure what to place in
    the designated spaces. We will need to work on responsiveness for these
    images.

--- src/routes/HomePage.svelte
+++ src/routes/HomePage.svelte
@@ -2,44 +2,109 @@
 	import Hero from "../Components/Hero.svelte";
 	import Container from "../Components/Container.svelte";
 	import ContainerCol from "../Components/ContainerCol.svelte";
-	import Footer from "../Components/Footer.svelte"; 
     import Article from "../Components/Article.svelte";
 	import ArticleCol from "../Components/ArticleCol.svelte";
 </script>
 
 <Hero/>
 
+<Container>
+	<Article>
+		<h1>Data Science Academy @ UCR</h1>
+		<br/>
+		<p>
+			Hosted at the University of California, Riverside, the Data Science Academy was started by professor Paea LePendu. Our aim is to introduce one of
+			the most popular and insightful field in Computer Science to <b>K-12 students</b>. Please read more about this program in the
+			<a href="/about">About DSA</a> page.
+			
+			<br/><br/>
+			Currently, we are hoping to host a new Data Science Academy cohort during the 2022-23 school year.
+		</p>
+
+	</Article>
+</Container>
+
 <ContainerCol>
 
 	<ArticleCol>
-		<h1> Title of the Article </h1>
+		<h1> Lessons </h1>
+		<br/>
+		<p>
+			We host a variety of lessons throughout our program. With these lessons, we aim to instill the importance of learning Data Science, privacy,
+			as well as many skills needed to manipulate and analyze datasets.
+		</p>
+
+		<br/>
+
+		<img src="https://placehold.co/600x400" alt="Placeholder">
 
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Tellus elementum sagittis vitae et leo duis ut diam. Vel turpis nunc eget lorem. Amet dictum sit amet justo donec enim diam vulputate. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Cras tincidunt lobortis feugiat vivamus at augue. Cursus eget nunc scelerisque viverra mauris. Pharetra magna ac placerat vestibulum lectus. Facilisi nullam vehicula ipsum a arcu cursus vitae congue.
-
-			Mattis nunc sed blandit libero volutpat sed cras. Aliquet risus feugiat in ante metus. Amet risus nullam eget felis. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Et tortor consequat id porta. Aliquet sagittis id consectetur purus ut. Sed turpis tincidunt id aliquet risus feugiat in. Leo duis ut diam quam nulla porttitor. Tincidunt vitae semper quis lectus nulla at volutpat. Tellus mauris a diam maecenas sed. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. In nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-			
-			Integer quis auctor elit sed vulputate mi. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Habitant morbi tristique senectus et netus. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. Amet mauris commodo quis imperdiet massa tincidunt. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. At volutpat diam ut venenatis tellus in. A condimentum vitae sapien pellentesque habitant morbi. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus et. Sapien pellentesque habitant morbi tristique. At augue eget arcu dictum varius duis at consectetur lorem. In fermentum posuere urna nec. Eget gravida cum sociis natoque penatibus et magnis dis. Ut enim blandit volutpat maecenas volutpat. Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Odio ut enim blandit volutpat maecenas volutpat blandit. Vulputate dignissim suspendisse in est ante in nibh mauris.
-			
-			Ac tincidunt vitae semper quis lectus nulla at volutpat diam. Ut enim blandit volutpat maecenas volutpat. Vitae tempus quam pellentesque nec nam aliquam sem. Posuere lorem ipsum dolor sit. Scelerisque felis imperdiet proin fermentum leo vel. Nec tincidunt praesent semper feugiat nibh. Proin sed libero enim sed faucibus turpis in. Lectus quam id leo in vitae. Nisi est sit amet facilisis magna etiam. Arcu dictum varius duis at. Facilisis magna etiam tempor orci eu lobortis. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Semper quis lectus nulla at. Vitae congue eu consequat ac felis. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Nibh tellus molestie nunc non blandit massa enim nec. A arcu cursus vitae congue. Dictumst vestibulum rhoncus est pellentesque. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Fermentum dui faucibus in ornare quam viverra.
-			
-			Id semper risus in hendrerit gravida rutrum quisque. Interdum posuere lorem ipsum dolor sit amet. Viverra justo nec ultrices dui sapien eget mi proin sed. Felis imperdiet proin fermentum leo vel orci porta. Sem nulla pharetra diam sit. Lorem ipsum dolor sit amet. Duis at consectetur lorem donec massa. Ipsum a arcu cursus vitae. Nam libero justo laoreet sit. Aliquam nulla facilisi cras fermentum odio eu. Hac habitasse platea dictumst vestibulum rhoncus est. Donec et odio pellentesque diam volutpat commodo. Varius quam quisque id diam vel. Posuere sollicitudin aliquam ultrices sagittis orci. Sed odio morbi quis commodo odio aenean sed adipiscing. Lectus magna fringilla urna porttitor rhoncus dolor purus. Massa massa ultricies mi quis hendrerit.
+			These lessons are produced both by professor Paea LePendu, as well as some of our DSA Leaders. These lessons include skillsets such as basic Python
+			programming, learning how to analyze datasets, Machine Learning basics, word cloud generation, data visualization, as well as chat bots. With these
+			lessons, we aim to not only provide the breadth of knowledge that students need to succeed in a Data Science career, but we also wish to motivate
+			students to pursue a Computer Science or Data Science profession.
 		</p>
+
+		<br/>
+
+		<img src="https://placehold.co/600x400" alt="Placeholder">
+
 	</ArticleCol>
 	<ArticleCol>
-		<h1> Title of the Article </h1>
+		<h1> Activities </h1>
+
+		<br/>
 
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Tellus elementum sagittis vitae et leo duis ut diam. Vel turpis nunc eget lorem. Amet dictum sit amet justo donec enim diam vulputate. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Cras tincidunt lobortis feugiat vivamus at augue. Cursus eget nunc scelerisque viverra mauris. Pharetra magna ac placerat vestibulum lectus. Facilisi nullam vehicula ipsum a arcu cursus vitae congue.
-
-			Mattis nunc sed blandit libero volutpat sed cras. Aliquet risus feugiat in ante metus. Amet risus nullam eget felis. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Et tortor consequat id porta. Aliquet sagittis id consectetur purus ut. Sed turpis tincidunt id aliquet risus feugiat in. Leo duis ut diam quam nulla porttitor. Tincidunt vitae semper quis lectus nulla at volutpat. Tellus mauris a diam maecenas sed. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. In nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+			While learning is extremely important, we wholeheartedly believe that fun, relaxation, and reflection are equally valuable to absorb new concepts.
+			We therefore host a variety of activities for students to do in between the lessons and main Data Science learning. With these activities, we hope 
+			that students are able to get away from the seriousness of Data Science and programming, and reflect on how these concepts can be applied in a fun,
+			entertaining way.
 			
-			Integer quis auctor elit sed vulputate mi. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Habitant morbi tristique senectus et netus. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. Amet mauris commodo quis imperdiet massa tincidunt. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. At volutpat diam ut venenatis tellus in. A condimentum vitae sapien pellentesque habitant morbi. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus et. Sapien pellentesque habitant morbi tristique. At augue eget arcu dictum varius duis at consectetur lorem. In fermentum posuere urna nec. Eget gravida cum sociis natoque penatibus et magnis dis. Ut enim blandit volutpat maecenas volutpat. Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Odio ut enim blandit volutpat maecenas volutpat blandit. Vulputate dignissim suspendisse in est ante in nibh mauris.
-			
-			Ac tincidunt vitae semper quis lectus nulla at volutpat diam. Ut enim blandit volutpat maecenas volutpat. Vitae tempus quam pellentesque nec nam aliquam sem. Posuere lorem ipsum dolor sit. Scelerisque felis imperdiet proin fermentum leo vel. Nec tincidunt praesent semper feugiat nibh. Proin sed libero enim sed faucibus turpis in. Lectus quam id leo in vitae. Nisi est sit amet facilisis magna etiam. Arcu dictum varius duis at. Facilisis magna etiam tempor orci eu lobortis. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Semper quis lectus nulla at. Vitae congue eu consequat ac felis. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Nibh tellus molestie nunc non blandit massa enim nec. A arcu cursus vitae congue. Dictumst vestibulum rhoncus est pellentesque. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Fermentum dui faucibus in ornare quam viverra.
-			
-			Id semper risus in hendrerit gravida rutrum quisque. Interdum posuere lorem ipsum dolor sit amet. Viverra justo nec ultrices dui sapien eget mi proin sed. Felis imperdiet proin fermentum leo vel orci porta. Sem nulla pharetra diam sit. Lorem ipsum dolor sit amet. Duis at consectetur lorem donec massa. Ipsum a arcu cursus vitae. Nam libero justo laoreet sit. Aliquam nulla facilisi cras fermentum odio eu. Hac habitasse platea dictumst vestibulum rhoncus est. Donec et odio pellentesque diam volutpat commodo. Varius quam quisque id diam vel. Posuere sollicitudin aliquam ultrices sagittis orci. Sed odio morbi quis commodo odio aenean sed adipiscing. Lectus magna fringilla urna porttitor rhoncus dolor purus. Massa massa ultricies mi quis hendrerit.
 		</p>
+
+		<br/>
+
+		<img src="https://placehold.co/600x400" alt="Placeholder">
+
+		<p>
+			In the past, these activities have included fun icebreakers, tours of the University of California, Riverside, fun implementations of Data Science
+			concepts such as Google's Quickdraw, bingo, and many more. And while these activities do not take up much of our time in the program, they are
+			essential to ensure that studens remain interested in the field.
+		</p>
+
+		<br/>
+
+		<img src="https://placehold.co/600x400" alt="Placeholder">
+
 	</ArticleCol>
 
+</ContainerCol>
+
+<Container>
+	<Article>
+		<h1>Campus Tour</h1>
+	</Article>
+
+	<Article>
+		<h1>DSA Leaders</h1>
+	</Article>
+
+</Container>
+
+<Container>
+	<Article>
+		<h1>Student Testimonials</h1>
+		<br/>
+		<p>Testing</p>
+	</Article>
+</Container>
+
+<ContainerCol>
+	<ArticleCol>
+		<h2>Testimonial</h2>
+	</ArticleCol>
+	<ArticleCol>
+		<h2>Testimonial</h2>
+	</ArticleCol>
 </ContainerCol>
