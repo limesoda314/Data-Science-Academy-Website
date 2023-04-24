ecdc7b8ec458b20228031279f31c51e8a7a8a210
ivanneto02 <ivan.a.neto@hotmail.com>
2023-04-24 05:28:40 -0700

    First test of DSA images and captions
    It is starting to look good, but we need some work on the captions and
    the styling as well

--- src/Components/Hero.svelte
+++ src/Components/Hero.svelte
@@ -2,23 +2,35 @@
     import './../Style/Hero.css';
     import Carousel from 'svelte-carousel';
     import Img from './Images.svelte';
+
+    let images = [
+        { caption: "Winston Chung Hall Photoshoot", src: "images/hero/img1.jpg"},
+        { caption: "Professor LePendu teaching students about data cleaning", src: "images/hero/img2.jpg"},
+        { caption: "All three Data Science Summer 2022 programs at once!", src: "images/hero/img3.jpg"},
+        { caption: "Inpiration", src: "images/hero/img4.jpg"},
+        { caption: "Analysis", src: "images/hero/img5.jpg"},
+        { caption: "Collaboration", src: "images/hero/img6.jpg"},
+        { caption: "A tour of University of California, Riverside campus", src: "images/hero/img7.jpg"},
+        { caption: "Enjoying lunch among friends and peers", src: "images/hero/img8.jpg"}
+    ];
+
+    console.log(images);
+
 </script>
 
 <div class="banner" id="Home"> 
     <Carousel
         autoplay 
-        autoplayDuration={5000}
+        autoplayDuration={10000}
         let:showPrevPage
         let:showNextPage        
     >
         <div slot="prev" on:click={showPrevPage} class="custom-arrow custom-arrow-prev">
             <i />
         </div>
-        <Img caption = "San Francisco" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg" alt="San Francisco"/>
-        <Img caption = "Scotland" src="https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg" alt="Scotland"/>
-        <Img caption = "Darjeeling" src="https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg" alt="Darjeeling"/>
-        <Img caption = "Scotland"src="https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg" alt="Scotland"/>
-        <Img caption = "San Francisco"src="https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx" alt="San Francisco"/>
+        {#each images as image}
+            <Img caption={image.caption} src={image.src} alt="No Image" />
+        {/each}
         <div slot="next" on:click={showNextPage} class="custom-arrow custom-arrow-next">
             <i />
         </div>
