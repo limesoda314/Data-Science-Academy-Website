a5cb8ba2c3dc521f2cee3e3869a8540cc5aa39d4
ivanneto02 <ivan.a.neto@hotmail.com>
2023-05-03 15:39:27 -0700

    Adding contact page

---
+++ src/routes/ContactPage.svelte
@@ -0,0 +1,25 @@
+
+<script>
+	import Container from "../Components/Container.svelte";
+	import ContainerCol from "../Components/ContainerCol.svelte";
+    import Article from "../Components/Article.svelte";
+	import ArticleCol from "../Components/ArticleCol.svelte";
+    import ProfileCard from "../Components/ProfileCard.svelte";
+    import ProfileContainer from "../Components/ProfileContainer.svelte";
+</script>
+
+<Container>
+    <Article>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3308.6721877104205!2d-117.32864282380257!3d33.975264273186085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcae5acc23da79%3A0x69a1ddccec014f14!2sDepartment%20of%20Computer%20Science%20and%20Engineering!5e0!3m2!1sen!2sus!4v1683145783274!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </Article>
+
+    <Article>
+        DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING
+        <br/>
+        Winston Chung Hall, 
+        <br/>
+        MARLAN AND ROSEMARY BOURNS COLLEGE OF ENGINEERING
+        <br/>
+        Riverside, CA 92521
+    </Article>
+</Container>
